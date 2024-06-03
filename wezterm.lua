bf2e271d2e91112183d9e9fa898b8d89ecf00054
Maciej Spiechowicz <spm1fr@bosch.com>
2024-06-03 13:07:15 +0200

    added brighter colors

--- wezterm.lua
+++ wezterm.lua
@@ -7,6 +7,7 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 config.font = wezterm.font 'JetBrains Mono'
 config.font_size = 16.0
+config.bold_brightens_ansi_colors = true
 
 -- and finally, return the configuration to wezterm
 return config

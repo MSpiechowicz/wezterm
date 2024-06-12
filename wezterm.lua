ceb2604a4fece95a20be65587d6ec15f103786de
Maciej Spiechowicz <spm1fr@bosch.com>
2024-06-12 14:06:08 +0200

    update the default font

--- wezterm.lua
+++ wezterm.lua
@@ -5,7 +5,7 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
-config.font = wezterm.font 'JetBrains Mono'
+config.font = wezterm.font 'Fira Code'
 config.font_size = 16.0
 config.bold_brightens_ansi_colors = true
 
